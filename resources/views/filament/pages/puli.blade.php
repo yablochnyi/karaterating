125e83bd6d41e749fcc00c5147e79f86f840d244
yablochnyi <artem.yablochnyi@yandex.ru>
2024-11-14 14:10:22 +0100

    update

--- resources/views/filament/pages/puli.blade.php
+++ resources/views/filament/pages/puli.blade.php
@@ -6,7 +6,7 @@
 {{--                class="mt-2 filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">--}}
 {{--            Переместить участников--}}
 {{--        </button>--}}
-        <div class="bracket disable-image" >
+        <div class="bracket disable-image" wire:ignore>
 
             @php
                 // Определяем максимальный номер раунда, исключая бои за третье место
