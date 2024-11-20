7478bbb274bbbeac200d2f8d26ecfd6c2154f7d8
yablochnyi <artem.yablochnyi@yandex.ru>
2024-11-20 17:49:14 +0100

    updated

--- app/Filament/Resources/AgreementResource/Pages/EditAgreement.php
+++ app/Filament/Resources/AgreementResource/Pages/EditAgreement.php
@@ -13,7 +13,7 @@ class EditAgreement extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+//            Actions\DeleteAction::make(),
         ];
     }
 }
