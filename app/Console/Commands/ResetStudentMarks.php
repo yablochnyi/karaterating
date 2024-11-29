29fa8e13482a16dbd91046cd8bf5c47f2d72fa96
yablochnyi <artem.yablochnyi@yandex.ru>
2024-11-29 13:44:58 +0100

    updated

---
+++ app/Console/Commands/ResetStudentMarks.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ResetStudentMarks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reset:student-marks';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Удаляет подтверждение марок у всех учеников раз в год, 1 января';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Log::debug(true);
+//        User::query()->update(['is_success_brand' => false]);
+
+        $this->info('Подтверждения марок успешно сброшены у всех учеников.');
+    }
+}
